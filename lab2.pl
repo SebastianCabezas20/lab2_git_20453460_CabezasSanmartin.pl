87d36aba01ac161bb66e1aac765a5c1cbeda0bf6
Sebastian Cabezas <sebastian.cabezas@usach.cl>
2020-11-23 00:17:15 -0300

    Register

--- lab2.pl
+++ lab2.pl
@@ -13,7 +13,7 @@
 % TDA usuario
 %(username,pass,reputacion)
 %Constructor
-usuario(Nombre,Pass,[Nombre,Pass,0]).
+usuario(Nombre,Pass,[[Nombre,Pass,0]]).
 
 % TDA Pregunta
 % (ID ,Autor,Fecha,Pregunta,[Etiquetas])
@@ -44,16 +44,24 @@ stack([[[user1,pass1,0],[user2,pass2,0],[user2,pass2,0]],
  [[1, 1, 20-20-20, "respuesta1", [et1, et2, et3]],
   [2, 2, 20-20-20, "respuesta2", [et1, et2, et3]],
   [3, 3, 20-20-20, "respuesta3", [et1, et2, et3]]]]).
-stack2([[[user1,pass1,0],[user2,pass2,0],[user2,pass2,0]],
- [[1, sebastian, 20-20-20, "pregunta1?", [et1, et2, et3]]],
+stack2([[[user1,pass1,0],[user2,pass2,0],[user3,pass2,0]],
+ [[1,nombreGenerico, 20-20-20, "pregunta1?", [et1, et2, et3]]],
  [[1, 1, 20-20-20, "respuesta1", [et1, et2, et3]]]]).
 
 % FUNCION DE REGISTRO
+verificar([Username|_],Username).
+
+%agregarUsuario([],Username,Pass,A):-usuario(Username,Pass,A).
+agregarUsuario([Usuario],Username,Pass,[Usuario|A]):-not(verificar(Usuario,Username)),usuario(Username,Pass,A).
+agregarUsuario([Primer|Siguientes],Username,Pass,[Primer|ColaNueva]):-
+    not(verificar(Primer,Username)),agregarUsuario(Siguientes,Username,Pass,ColaNueva).
+
 register([A,B,C],Username,Pass,[P,B,C]):-agregarUsuario(A,Username,Pass,P).
 %
 %
 %
 %
+
 
 
 
